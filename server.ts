bb087ebf0057b3b9bcdc5baff45feb5acd9d7f36
vincelaird <vincelaird@gmail.com>
2024-03-14 01:16:02 -0400

    update server file

--- server.ts
+++ server.ts
@@ -3,13 +3,12 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+// Adjust the path for static files to go up one directory
+app.use(express.static(path.join(__dirname, '../build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// Adjust the path for the catchall handler as well
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(PORT, () => {
