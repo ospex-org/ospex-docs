00a03eeba020b9cb9762563cc1e514349108cc7e
vincelaird <vincelaird@gmail.com>
2024-03-14 01:04:19 -0400

    update start script to serve static files

--- server.ts
+++ server.ts
@@ -9,9 +9,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
